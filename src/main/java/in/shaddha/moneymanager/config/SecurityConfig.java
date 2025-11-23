95894d75118d3f64027251ce3eb12d3d129a8ee3
shraddha <shraddha@gmail.com>
2025-11-23 21:44:48 +0530

    Your commit message heredds

--- src/main/java/in/shaddha/moneymanager/config/SecurityConfig.java
+++ src/main/java/in/shaddha/moneymanager/config/SecurityConfig.java
@@ -67,16 +67,17 @@ public class SecurityConfig {
 
    @Bean
     public CorsConfigurationSource configurationSource(){
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));  // FIXED 🔥
-        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*")); 
-        configuration.setAllowCredentials(true);
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of("*"));   // 🔥 IMPORTANT
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/**", config);
         return source;
     }
+
 
     @Bean
     public AuthenticationManager authenticationManager(){
